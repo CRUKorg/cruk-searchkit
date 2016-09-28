7b2982b1236bc52bd1c7c9f535a6c41fd60a0bc9
Mirko Prpic <m.prpic@catchdigital.com>
2016-09-28 23:14:23 +0100

    Added main index.js file with all relevant exports

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+export { default as CRUKSearchInput } from './src/components/input/Input';
+export { default as CRUKSearch } from './src/components/search/Search';
+export { default as CRUKSearchNoResultsDisplay } from './src/components/noresults/NoResults';
+export { default as CRUKPagination } from './src/components/pagination/Pagination';
+export { default as CRUKSearchResult } from './src/components/result/Result';
+export { default as CRUKSearchSummary } from './src/components/summary/Summary';
+export { default as CRUKLoading } from './src/components/loading/Loading';
+export { default as CRUKHitsList } from './src/components/hitsList/HitsList';
+export { default as CRUKSearchHits } from './src/components/hits/Hits';
+export { default as CRUKSearchGTM } from './src/components/gtm/GTM';
+export { CRUKSearchkitCSS } from './src/styles/common.scss';
