981bf5170071cbd2963eed7542b7e6453d3a5cfc
Mirko Prpic <m.prpic@catchdigital.com>
2016-10-12 11:58:44 +0100

    Removed console.log

--- src/components/geosuggest/CRUKGeoSuggestAccessor.jsx
+++ src/components/geosuggest/CRUKGeoSuggestAccessor.jsx
@@ -20,7 +20,6 @@ export class CRUKGeoSuggestAccessor extends FilterBasedAccessor {
 
   buildSharedQuery(query) {
     const val = this.state.getValue()
-    console.log(this.options)
     if(Object.keys(val).length > 0){
       let filter = {
         geo_distance: {
