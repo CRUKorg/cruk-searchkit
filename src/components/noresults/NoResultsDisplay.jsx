a84f5d34ea739c8e42c024af809854c46282e80d
Mirko Prpic <m.prpic@catchdigital.com>
2016-10-21 12:50:35 +0100

    Changed the logic for np-result title and body

--- src/components/noresults/NoResultsDisplay.jsx
+++ src/components/noresults/NoResultsDisplay.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
 
 export default class CRUKNoResultsDisplay extends React.Component {
+
   render() {
     const {bemBlocks, query, suggestion} = this.props
 
-    let noResultsBody = (
-      <div>
-        <p>Suggestions:</p>
-        <ul>
-          <li>Check your spelling</li>
-          <li>Try different or more general words</li>
-        </ul>
-      </div>
-    )
-
-    let noResultsTitle = "We didn't find any results for your search"
+    const queryLabel = (query)
+      ? <span> - <strong>{query}</strong></span>
+      : ''
     
-    noResultsBody = (this.props.noResultsBody) ? this.props.noResultsBody : noResultsBody;
-    noResultsTitle = (this.props.noResultsTitle) ? this.props.noResultsTitle : noResultsTitle;
+    const noResultsBody = (() => {
+      if (this.props.noResultsBody === '') return ''
+      if (this.props.noResultsBody) return this.props.noResultsBody
+      return (
+        <div>
+          <p>Suggestions:</p>
+          <ul>
+            <li>Check your spelling</li>
+            <li>Try different or more general words</li>
+          </ul>
+        </div>
+      )
+    })()
+
+    const noResultsTitle = (() => {
+      if (this.props.noResultsTitle === '') return ''
+      if (this.props.noResultsTitle) return <h2 className="no-results__heading">{this.props.noResultsTitle}{queryLabel}</h2>
+      return <h2 className="no-results__heading">We didn't find any results for your search{queryLabel}</h2>
+    })()
 
     return (
       <div data-qa="no-hits" className={bemBlocks.container()}>
         <div className={bemBlocks.container("info")}>
-          <h2 className="no-results__heading">{noResultsTitle} - <strong>{query}</strong></h2>
+          {noResultsTitle}
           {noResultsBody}
         </div>
       </div>
