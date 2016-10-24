e1876e441214d1483acb34be9e5987d91c861919
heylookalive <allister.price@gmail.com>
2016-10-24 15:28:31 +0100

    Renamed CRUKSearchGTM to CRUKSearchkitGTM.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export { default as CRUKSearchkitSummary } from './src/components/summary/CRUKSe
 export { default as CRUKSearchkitLoading } from './src/components/loading/CRUKSearchkitLoading';
 export { default as CRUKHitsList } from './src/components/hitsList/HitsList';
 export { default as CRUKSearchHits } from './src/components/hits/Hits';
-export { default as CRUKSearchGTM } from './src/components/gtm/GTM';
+export { default as CRUKSearchkitGTM } from './src/components/gtm/CRUKSearchkitGTM';
 export { default as CRUKDateRange } from './src/components/date/CRUKDateRange';
 export { default as CRUKGeoSuggest } from './src/components/geosuggest/CRUKGeoSuggest';
 export { default as CRUKSearchkitSelect } from './src/components/ui/list/CRUKSearchkitSelect';
