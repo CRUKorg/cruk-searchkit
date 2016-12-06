d315f0a37c6f2bc016a9b916388570fe16826047
Mirko Prpic <m.prpic@catchdigital.com>
2016-12-06 15:44:07 +0000

    Changed test description

--- src/components/display/help-text/tests/CRUKSearchkitHelpText.unit.jsx
+++ src/components/display/help-text/tests/CRUKSearchkitHelpText.unit.jsx
@@ -3,7 +3,7 @@ import { mount } from 'enzyme';
 import { SearchkitManager } from 'searchkit';
 import CRUKSearchkitHelpText from '../CRUKSearchkitHelpText.jsx';
 
-describe('Cruk searchkit help-text tests', () => {
+describe('Cruk searchkit CRUKSearchkitHelpText component tests', () => {
 
   beforeEach(function() {
     this.searchkit = SearchkitManager.mock();
