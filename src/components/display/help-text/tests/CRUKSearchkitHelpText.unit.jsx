2bead58075a735c8ee550fd54ecac22c90fda124
Mirko Prpic <m.prpic@catchdigital.com>
2016-12-06 14:29:45 +0000

    Added unit tests for CRUKSearchkitHelpText component

---
+++ src/components/display/help-text/tests/CRUKSearchkitHelpText.unit.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { SearchkitManager } from 'searchkit';
+import CRUKSearchkitHelpText from '../CRUKSearchkitHelpText.jsx';
+
+describe('Cruk searchkit help-text tests', () => {
+
+  beforeEach(function() {
+    this.searchkit = SearchkitManager.mock();
+    this.createWrapper = (helptext) => {
+      this.wrapper = mount(
+        <CRUKSearchkitHelpText
+          searchkit={this.searchkit}
+          helptext={helptext}
+        />
+      );
+    }
+  });
+
+  it('Render with default help text', function() {
+    this.createWrapper();
+    expect(this.wrapper.find('h2').text()).toBe('Please enter a keyword in the text box to start searching.');
+  });
+
+  it('Render with custom help text', function() {
+    const helpJsx = <h1 className="crazy-class">
+        This is <strong>custom help text</strong>
+      </h1>;
+    this.createWrapper(helpJsx);
+    expect(this.wrapper.render().find('.crazy-class > strong').text()).toBe('custom help text');
+  });
+});
