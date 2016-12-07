7497ef985bb07e63ca5efc8beefd4d316a0b8eed
Mirko Prpic <m.prpic@catchdigital.com>
2016-12-07 09:59:16 +0000

    Added click handler

--- src/components/display/alert/CRUKSearchkitAlert.jsx
+++ src/components/display/alert/CRUKSearchkitAlert.jsx
@@ -16,17 +16,30 @@ export default class CRUKSearchkitAlert extends React.Component {
     dismissable: false
   }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: true
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState({visible: false});
+  }
+
   render() {
-    const dismissableClass = this.props.dismissable ? ' cr-hu-alert--dismissible' : ''
-    const animationClass = this.props.animation ? ` cr-animated-${this.props.animation}` : ''
-    const cssClasses = `cr-hu-alert cr-hu-alert--${this.props.type}${dismissableClass}${animationClass}`; 
+    const dismissableClass = this.props.dismissable ? ' cr-hu-alert--dismissible' : '';
+    const animationClass = this.props.animation ? ` cr-animated-${this.props.animation}` : '';
+    const hiddenClass = !this.state.visible ? ' hidden' : '';
+    const cssClasses = `cr-hu-alert cr-hu-alert--${this.props.type}${dismissableClass}${animationClass}${hiddenClass}`; 
     return (
       <div className={cssClasses} role="alert">
         <i className="cr-hu-alert__icon" aria-hidden="true"></i>
         <div className="cr-hu-alert__text">
           {this.props.text}
         </div>
-        { this.props.dismissable && <button type="button" className="cr-hu-alert__dismiss" aria-label="Dismiss"></button> }
+        { this.props.dismissable && <button type="button" className="cr-hu-alert__dismiss" onClick={this.handleClick} aria-label="Dismiss"></button> }
       </div>
     );
   }
