1cb17b244b18edf162ce2bab9a2577f0a44d9fc9
Mirko Prpic <m.prpic@catchdigital.com>
2016-12-07 10:30:27 +0000

    Added cookie management to track closing of alerts

--- src/components/display/alert/CRUKSearchkitAlert.jsx
+++ src/components/display/alert/CRUKSearchkitAlert.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import cookie from 'react-cookie';
 
 /**
  * Export our CRUKSearchkitAlert component.
  */
 export default class CRUKSearchkitAlert extends React.Component {
   static propTypes = {
+    id: React.PropTypes.string.isRequired,
     type: React.PropTypes.string,
     text: React.PropTypes.string.isRequired,
     dismissable: React.PropTypes.bool,
@@ -19,19 +21,20 @@ export default class CRUKSearchkitAlert extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      visible: true
+      invisible: cookie.load(`cru-hu-alert-${this.props.id}`)
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(e) {
-    this.setState({visible: false});
+    this.setState({invisible: true});
+    cookie.save(`cru-hu-alert-${this.props.id}`, true);
   }
 
   render() {
     const dismissableClass = this.props.dismissable ? ' cr-hu-alert--dismissible' : '';
     const animationClass = this.props.animation ? ` cr-animated-${this.props.animation}` : '';
-    const hiddenClass = !this.state.visible ? ' hidden' : '';
+    const hiddenClass = this.state.invisible ? ' hidden' : '';
     const cssClasses = `cr-hu-alert cr-hu-alert--${this.props.type}${dismissableClass}${animationClass}${hiddenClass}`; 
     return (
       <div className={cssClasses} role="alert">
