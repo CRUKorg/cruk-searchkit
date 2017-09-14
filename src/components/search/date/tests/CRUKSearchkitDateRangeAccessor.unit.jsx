e201b24edb2c5002c18a3ce9c042587c1038b87a
David Hughes <david.hughes125@gmail.com>
2017-09-14 12:28:25 +0100

    DIG-1294: Update DateRangeAccessor unit tests.

--- src/components/search/date/tests/CRUKSearchkitDateRangeAccessor.unit.jsx
+++ src/components/search/date/tests/CRUKSearchkitDateRangeAccessor.unit.jsx
@@ -22,6 +22,16 @@ describe('CRUKSearchkitDateRangeAccessor tests', () => {
       updateParentState: () => null
     };
 
+    this.multipleDayOptions = {
+      id: 'date-filter',
+      startDateField: 'date_start',
+      endDateField: 'date_end',
+      title:'Date filter',
+      startDate: '2016-08-12',
+      endDate: '2016-08-15',
+      updateParentState: () => null
+    };
+
     this.accessor = new CRUKSearchkitDateRangeAccessor('date-filter', this.options);
 
   });
@@ -52,6 +62,76 @@ describe('CRUKSearchkitDateRangeAccessor tests', () => {
     });
   });
 
+  it('buildSharedQuery with specified start & end date', function() {
+    this.accessor = new CRUKSearchkitDateRangeAccessor('date-filter', this.multipleDayOptions);
+
+    let query = new ImmutableQuery();
+    query = this.accessor.buildSharedQuery(query);
+    let filters = query.getFilters();
+    expect(toPlainObject(filters)).toEqual({});
+
+    this.accessor.state = this.accessor.state.setValue({
+      min: '2016-08-12',
+      max: '2016-08-15'
+    });
+
+    query = new ImmutableQuery();
+    query = this.accessor.buildSharedQuery(query);
+    filters = query.getFilters();
+
+    expect(toPlainObject(filters)).toEqual({
+      bool: {
+        should: [{
+          range: {
+            date_start: {
+              gte: '2016-08-12',
+              lte: '2016-08-16',
+              format: 'yyyy-MM-dd'
+            }
+          }
+        }, {
+          range: {
+            date_end: {
+              gte: '2016-08-12',
+              lte: '2016-08-16',
+              format: 'yyyy-MM-dd'
+            }
+          }
+        }, {
+          bool: {
+            must: [{
+              range: {
+                date_start: {
+                  lt: '2016-08-12',
+                  format: 'yyyy-MM-dd'
+                }
+              }
+            }, {
+              range: {
+                date_end: {
+                  gt: '2016-08-16',
+                  format: 'yyyy-MM-dd'
+                }
+              }
+            }]
+          }
+        }, {
+          bool: {
+            must_not: [{
+              exists: {
+                field: 'date_start'
+              }
+            }, {
+              exists: {
+                field: 'date_end'
+              }
+            }]
+          }
+        }]
+      }
+    });
+  });
+
   it('buildOwnQuery', function() {
     let query = new ImmutableQuery();
     this.accessor.state = this.accessor.state.setValue({
@@ -69,6 +149,31 @@ describe('CRUKSearchkitDateRangeAccessor tests', () => {
       })
     ]);
     const metric = CardinalityMetric(this.accessor.key, this.accessor.options.field);
+
+    expect(toPlainObject(query.query.aggs)).toEqual(
+      FilterBucket(this.accessor.key, filters, metric)
+    );
+  });
+
+  it('buildOwnQuery with specified start & end date', function() {
+    this.accessor = new CRUKSearchkitDateRangeAccessor('date-filter', this.multipleDayOptions);
+
+    let query = new ImmutableQuery();
+    this.accessor.state = this.accessor.state.setValue({
+      min: '2016-08-12',
+      max: '2016-08-15'
+    });
+    query = this.accessor.buildOwnQuery(query);
+
+    // Comparison data.
+    const filters = BoolMust([
+      RangeQuery(this.accessor.options.startDateField, {
+        gte: '2016-08-12',
+        lt: this.accessor.addOneDay('2016-08-15'),
+        format: 'yyyy-MM-dd'
+      })
+    ]);
+    const metric = CardinalityMetric(this.accessor.key, this.accessor.options.startDateField);
 
     expect(toPlainObject(query.query.aggs)).toEqual(
       FilterBucket(this.accessor.key, filters, metric)
