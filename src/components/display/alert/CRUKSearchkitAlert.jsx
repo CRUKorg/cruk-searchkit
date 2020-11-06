142ce5bdd955502e11e0f86ef097a7d5c953a330
David Hughes <david.hughes@cancer.org.uk>
2020-11-06 12:30:02 +0000

    RFLW-146: ESLint - trying.

--- src/components/display/alert/CRUKSearchkitAlert.jsx
+++ src/components/display/alert/CRUKSearchkitAlert.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import cookie from 'react-cookie';
-import {
-  SearchkitComponent
-} from "searchkit";
+import { SearchkitComponent } from 'searchkit';
 
 /**
  * Export our CRUKSearchkitAlert component.
@@ -14,46 +12,55 @@ export default class CRUKSearchkitAlert extends SearchkitComponent {
     text: React.PropTypes.object.isRequired,
     dismissable: React.PropTypes.bool,
     animation: React.PropTypes.string
-  }
+  };
 
   static defaultProps = {
     type: 'warning',
     dismissable: false
-  }
+  };
 
   constructor(props) {
     super(props);
     this.state = {
       invisible: cookie.load(`cru-hu-alert-${this.props.id}`)
-    }
+    };
     this.stopAnimation = false;
 
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(e) {
-    this.setState({invisible: true});
+  handleClick() {
+    this.setState({ invisible: true });
     cookie.save(`cru-hu-alert-${this.props.id}`, true);
   }
 
   componentDidMount() {
-    this.searchkit.addResultsListener((result) => {
+    this.searchkit.addResultsListener(() => {
       this.stopAnimation = true;
     });
   }
 
   render() {
-    if (this.state.invisible || this.isLoading()) return null;
-    const dismissableClass = this.props.dismissable ? ' cr-hu-alert--dismissible' : '';
-    const animationClass = (this.props.animation && !this.stopAnimation) ? ` cr-animated-${this.props.animation}` : '';
-    const cssClasses = `cr-hu-alert cr-hu-alert--${this.props.type}${dismissableClass}${animationClass}`; 
+    if (this.state.invisible || this.isLoading()) {
+      return null;
+    }
+    const { dismissable, animation, type } = this.props;
+    const dismissableClass = dismissable ? ' cr-hu-alert--dismissible' : '';
+    const animationClass = (animation && !this.stopAnimation) ?
+        ` cr-animated-${animation}` : '';
+    const cssClasses = `cr-hu-alert cr-hu-alert--${type}${dismissableClass}${animationClass}`;
     return (
       <div id={this.props.id} className={cssClasses} role="alert">
-        <i className="cr-hu-alert__icon" aria-hidden="true"></i>
+        <i className="cr-hu-alert__icon" aria-hidden="true" />
         <div className="cr-hu-alert__text">
           {this.props.text}
         </div>
-        { this.props.dismissable && <button type="button" className="cr-hu-alert__dismiss" onClick={this.handleClick} aria-label="Dismiss"></button> }
+        { this.props.dismissable && <button
+          type="button"
+          className="cr-hu-alert__dismiss"
+          onClick={this.handleClick}
+          aria-label="Dismiss"
+        /> }
       </div>
     );
   }
--- src/components/search/date/CRUKSearchkitDateRange.jsx
+++ src/components/search/date/CRUKSearchkitDateRange.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { DateRangePicker } from 'react-dates';
 import moment from 'moment';
-import {
-  QueryAccessor,
-  SearchkitComponent,
-  SearchkitComponentProps,
-  ReactComponentType,
-  renderComponent
-} from 'searchkit';
 import MobileDetect from 'mobile-detect';
-import {
-  CRUKSearchkitDateRangeAccessor
-} from './CRUKSearchkitDateRangeAccessor';
-
-const isUndefined = require('lodash/isUndefined');
+import { DateRangePicker } from 'react-dates';
+import { SearchkitComponent } from 'searchkit';
+import { CRUKSearchkitDateRangeAccessor } from './CRUKSearchkitDateRangeAccessor';
 
 class CRUKSearchkitDateRange extends SearchkitComponent {
-  accessor:CRUKSearchkitDateRangeAccessor
-
   static propTypes = {
     startDate: React.PropTypes.object,
     endDate: React.PropTypes.object,
@@ -33,21 +21,20 @@ class CRUKSearchkitDateRange extends SearchkitComponent {
     endDateField: React.PropTypes.string,
     id: React.PropTypes.string,
     ...SearchkitComponent.propTypes
-  }
+  };
 
   constructor(props) {
-    const startDate = (props.startDate) ? props.startDate : null
-    const endDate = (props.endDate) ? props.endDate : null
-    super(props)
+    super(props);
 
     this.state = {
       focusedInput: null,
-      startDate: startDate,
-      endDate: endDate,
-      initialVisibleMonth: () => moment()
-    }
+      startDate: props.startDate || null,
+      endDate: props.endDate || null,
+      initialVisibleMonth: moment
+    };
 
-    this.noArgs = true
+    this.noArgs = true;
+    this.accessor = this.defineAccessor();
 
     this.onDatesChange = this.onDatesChange.bind(this);
     this.onFocusChange = this.onFocusChange.bind(this);
@@ -55,51 +42,66 @@ class CRUKSearchkitDateRange extends SearchkitComponent {
   }
 
   onDatesChange({ startDate, endDate }) {
-
-    this.setState({ startDate: startDate, endDate: endDate }, () => {
+    this.setState({ startDate, endDate }, () => {
       this.updateAccessorState(startDate, endDate);
-    })
+    });
 
-    this.noArgs = false
+    this.noArgs = false;
   }
 
   updateAccessorState(startDate, endDate) {
     if (startDate && endDate) {
       this.accessor.state = this.accessor.state.setValue({
-        min: startDate.format("YYYY-MM-DD"),
-        max: endDate.format("YYYY-MM-DD")
-      })
-      this.searchkit.performSearch()
+        min: startDate.format('YYYY-MM-DD'),
+        max: endDate.format('YYYY-MM-DD')
+      });
+      this.searchkit.performSearch();
     } else if (!startDate && !endDate) {
-      this.accessor.state = this.accessor.state.clear()
-      this.searchkit.performSearch()
+      this.accessor.state = this.accessor.state.clear();
+      this.searchkit.performSearch();
     }
-
   }
 
   updateParentState(startDate, endDate) {
     this.setState({
       startDate: (startDate) ? moment(startDate) : startDate,
       endDate: (endDate) ? moment(endDate) : endDate,
-    })
+    });
   }
 
   onFocusChange(focusedInput) {
-    this.setState({ focusedInput })
+    this.setState({ focusedInput });
   }
 
   defineAccessor() {
-    const { id, title, field, startDate, endDate,
-      interval, showHistogram, startDateField, endDateField } = this.props;
+    const {
+      id,
+      title,
+      field,
+      startDate,
+      endDate,
+      interval,
+      startDateField,
+      endDateField
+    } = this.props;
     const updateParentState = this.updateParentState;
-    return new CRUKSearchkitDateRangeAccessor(id,{
-      id, startDate, endDate, title, field, interval, updateParentState, startDateField, endDateField
-    })
+    return new CRUKSearchkitDateRangeAccessor(id, {
+      id,
+      startDate,
+      endDate,
+      title,
+      field,
+      interval,
+      updateParentState,
+      startDateField,
+      endDateField
+    });
   }
 
   render() {
     const argState = this.accessor.getQueryObject();
-    let { focusedInput, startDate, endDate, initialVisibleMonth } = this.state;
+    const { focusedInput } = this.state;
+    let { startDate, endDate, initialVisibleMonth } = this.state;
     /**
      * These functions can return null so check for bool true.
      */
@@ -107,12 +109,14 @@ class CRUKSearchkitDateRange extends SearchkitComponent {
     const isMobile = md.mobile() || md.phone();
     const numberOfMonths = isMobile ? 1 : 2;
 
-    if (this.noArgs && argState[this.props.id] !== undefined && Object.keys(argState[this.props.id]).length > 0) {
+    if (this.noArgs &&
+        argState[this.props.id] !== undefined &&
+        Object.keys(argState[this.props.id]).length > 0) {
       if (typeof argState[this.props.id].min !== undefined) {
-        startDate =  moment(argState[this.props.id].min)
-        initialVisibleMonth = () => startDate
+        startDate = moment(argState[this.props.id].min);
+        initialVisibleMonth = () => startDate;
       }
-      endDate = (typeof argState[this.props.id].max !== undefined) ? moment(argState[this.props.id].max) : null
+      endDate = (typeof argState[this.props.id].max !== undefined) ? moment(argState[this.props.id].max) : null;
     }
 
     return (
@@ -129,12 +133,12 @@ class CRUKSearchkitDateRange extends SearchkitComponent {
           numberOfMonths={numberOfMonths}
         />
       </div>
-    )
+    );
   }
 }
 
 CRUKSearchkitDateRange.defaultProps = {
   isOutsideRange: () => false
-}
+};
 
 export default CRUKSearchkitDateRange;
